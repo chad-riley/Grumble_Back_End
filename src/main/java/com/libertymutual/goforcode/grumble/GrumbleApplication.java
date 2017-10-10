b4eff4f44dd627161fdaca2c20de82bb9e29ae38
Chad Riley <chadwick.riley@libertymutual.com>
2017-10-10 08:27:44 -0700

    starting point

---
+++ src/main/java/com/libertymutual/goforcode/grumble/GrumbleApplication.java
@@ -0,0 +1,12 @@
+package com.libertymutual.goforcode.grumble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrumbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrumbleApplication.class, args);
+	}
+}
