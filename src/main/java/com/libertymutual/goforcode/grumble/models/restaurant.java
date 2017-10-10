8a1829c8d62cafcd93591e99d5eb486f783179c4
Chad Riley <chadwick.riley@libertymutual.com>
2017-10-10 09:36:49 -0700

    added restaurant model

--- src/main/java/com/libertymutual/goforcode/grumble/models/restaurant.java
+++ src/main/java/com/libertymutual/goforcode/grumble/models/restaurant.java
@@ -1,5 +1,67 @@
 package com.libertymutual.goforcode.grumble.models;
 
-public class restaurant {
+import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class,
+		property="id")
+@Entity
+public class Restaurant {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(length=255, nullable=false)
+	private String restaurantApiKey;
+	
+	@Column(length=255, nullable=false)
+	private String restaurantName;
+	
+	@OneToMany(mappedBy="restaurant", cascade=CascadeType.ALL)
+	private List<MenuItems> menuItems;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRestaurantApiKey() {
+		return restaurantApiKey;
+	}
+
+	public void setRestaurantApiKey(String restaurantApiKey) {
+		this.restaurantApiKey = restaurantApiKey;
+	}
+
+	public String getRestaurantName() {
+		return restaurantName;
+	}
+
+	public void setRestaurantName(String restaurantName) {
+		this.restaurantName = restaurantName;
+	}
+
+	public List<MenuItems> getMenuItems() {
+		return menuItems;
+	}
+
+	public void setMenuItems(List<MenuItems> menuItems) {
+		this.menuItems = menuItems;
+	}
+	
 }
