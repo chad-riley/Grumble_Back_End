01e41346a68390250874b3a37fff01491be5b04c
Trace Ferre <trace.ferre@libertymutual.com>
2017-10-11 09:19:04 -0700

    Adding bean to enable CORS

--- src/main/java/com/libertymutual/goforcode/grumble/GrumbleApplication.java
+++ src/main/java/com/libertymutual/goforcode/grumble/GrumbleApplication.java
@@ -2,6 +2,10 @@ package com.libertymutual.goforcode.grumble;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class GrumbleApplication {
@@ -9,4 +13,14 @@ public class GrumbleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GrumbleApplication.class, args);
 	}
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/*").allowedOrigins("*");
+            }
+        };
+    }
 }
